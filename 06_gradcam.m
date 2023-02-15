9c479f33ce26b24ecfc75bc5198ba063a2930cd1
Yudai Tanaka <63273927+yudaitanaka1026@users.noreply.github.com>
2023-02-15 13:03:28 +0900

    Add files via upload

--- 06_gradcam.m
+++ 06_gradcam.m
@@ -3,3 +3,28 @@
 
 Apply Grad-CAM to CNN
 %}
+
+%Read the fine-tuned network
+net = coder.loadDeepLearningNetwork("*****.mat");
+inputSize = net.Layers(1).InputSize(1:2);
+%Resize image size
+img = imread("*****.bmp");
+img = imresize(img,inputSize);
+%Classify the image
+[classfn,score] = classify(net,img)
+%Compute the Grad-CAM map
+map = gradCAM(net,img,classfn);
+%Show the Grad-CAM map
+imshow(img);
+hold on;
+imagesc(map,'AlphaData',0.5);
+colormap jet
+hold off;
+title("Grad-CAM");
+%Save the Grad-CAM map
+output = map;
+writematrix(output,['*****.csv']);
+
+
+
+
