ec3039e9e499a708dea18364cba036344317db69
Yudai Tanaka <63273927+yudaitanaka1026@users.noreply.github.com>
2023-05-29 10:54:27 +0900

    Update 05_train-squeezenet.m

--- 05_train-squeezenet.m
+++ 05_train-squeezenet.m
@@ -4,30 +4,26 @@
 Transfer learning with SqueezeNet
 %}
 
-%Load dataset
-imds = imageDatastore('*****', ...
-    'IncludeSubfolders',true, ...
-    'LabelSource','foldernames');
-%Divide the data into training and validation datasets
-[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7,'randomized')
-%Record image names of each datasets
-writecell(imdsTrain.Files,'*****.csv');
-writecell(imdsValidation.Files,'*****.csv');
-%Load pretrained network
+% Load dataset
+imds = imageDatastore('***','IncludeSubfolders',true, 'LabelSource','foldernames');
+
+% Divide the data into training, validation, and test datasets
+[imdsRest, imdsTest] = splitEachLabel(imds, 0.8, 'randomized');
+[imdsTrain, imdsValidation] = splitEachLabel(imdsRest, 0.8, 'randomized');
+
+% Load pretrained network
 net = squeezenet;
-net.Layers(1)
 inputSize = net.Layers(1).InputSize;
-%Replace final layers
+
+% Replace final layers
 lgraph = layerGraph(net);
 [learnableLayer,classLayer] = findLayersToReplace(lgraph);
-[learnableLayer,classLayer]
 numClasses = numel(categories(imdsTrain.Labels));
 if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
     newLearnableLayer = fullyConnectedLayer(numClasses, ...
         'Name','new_fc', ...
         'WeightLearnRateFactor',10, ...
         'BiasLearnRateFactor',10);
-    
 elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
     newLearnableLayer = convolution2dLayer(1,numClasses, ...
         'Name','new_conv', ...
@@ -37,16 +33,14 @@ end
 lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
 newClassLayer = classificationLayer('Name','new_classoutput');
 lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
-figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
-plot(lgraph)
-ylim([0,10])
-%Freeze initial layers
+
+% Freeze initial layers
 layers = lgraph.Layers;
 connections = lgraph.Connections;
 layers(1:4) = freezeWeights(layers(1:4));
 lgraph = createLgraphUsingConnections(layers,connections);
-analyzeNetwork(lgraph)
-%Data augmentation
+
+% Data augmentation
 pixelRange = [-30 30];
 imageAugmenter = imageDataAugmenter( ...
     'RandXReflection',true, ...
@@ -54,8 +48,10 @@ imageAugmenter = imageDataAugmenter( ...
     'RandYTranslation',pixelRange);
 augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
     'DataAugmentation',imageAugmenter);
-augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
-%Specify the training options
+augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation, ...
+    'DataAugmentation',imageAugmenter);
+
+% Specify the training options
 miniBatchSize = 512;
 valFrequency = floor(numel(augimdsTrain.Files)/miniBatchSize);
 options = trainingOptions('sgdm', ...
@@ -67,14 +63,32 @@ options = trainingOptions('sgdm', ...
     'ValidationFrequency',valFrequency, ...
     'Verbose',false, ...
     'Plots','training-progress');
-%Train the replaced network
+
+% Train the network
 [netTransfer,info] = trainNetwork(augimdsTrain,lgraph,options);
-%Classify validation images
-[YPred,scores] = classify(netTransfer,augimdsValidation);
-YValidation = imdsValidation.Labels;
-accuracy = mean(YPred == YValidation)
-%save model
-save("*****.mat","netTransfer")
-%save the history of accuracy and loss
-output = [info.TrainingAccuracy;info.ValidationAccuracy;info.TrainingLoss;info.ValidationLoss]';
-writematrix(output,"*****.csv");
+
+% Save the model
+save("***","netTransfer");
+
+% Save the history of accuracy and loss
+output = [info.TrainingAccuracy; info.ValidationAccuracy; info.TrainingLoss; info.ValidationLoss], writematrix(output, "***");
+
+% Prepare test data for evaluation
+augimdsTest = augmentedImageDatastore(inputSize(1:2),imdsTest);
+
+% Classify test images
+[YPred,scores] = classify(netTransfer,augimdsTest);
+YTest = imdsTest.Labels;
+
+% Compute the confusion matrix
+confMatrix = confusionmat(YTest, YPred);
+
+% Save the confusion matrix as a CSV file
+writematrix(confMatrix, "***");
+
+% Calculate final accuracy on test data
+final_test_accuracy = mean(YPred == YTest);
+
+% Save the final results on test data
+writematrix(final_test_accuracy, '***');
+save('***', 'YPred');
