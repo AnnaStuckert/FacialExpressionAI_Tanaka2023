0929727cc73a5d12b27073044ff51bdf6b7bd4cc
AnnaStuckert <47814177+AnnaStuckert@users.noreply.github.com>
2025-01-27 10:17:33 +0100

    updated 02_mask.py to work on Mac

--- 01_mov2pic_Not_Relevant.py
+++ 01_mov2pic_Not_Relevant.py
@@ -8,21 +8,22 @@ Created on Tue Apr  5 19:21:38 2022
 Convert a multi-tiff file to a series of BMP format files.
 """
 
-# Import modules
-!pip install gputil
-!pip install psutil
-!pip install humanize
-import psutil
-import humanize
 import os
+import random
+
+import cv2
 import GPUtil as GPU
-import os
-from PIL import Image
+import humanize
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-import random
+
+# Import modules
+#!pip install gputil
+#!pip install psutil
+#!pip install humanize
+import psutil
 from google.colab.patches import cv2_imshow
+from PIL import Image
 
 # Get filenames of the input movies
 BASENAME = []
@@ -34,19 +35,20 @@ BASENAME.append(BASENAME2)
 "......."
 
 # Set the path
-PATH = 'drive/My Drive/data/'
+PATH = "drive/My Drive/data/"
 BASENAME = "******"
 
 # Set the size of photos
 im_rows = 600
 im_cols = 700
-im_rows = int(im_rows*0.33) 
-im_cols = int(im_cols*0.33)
+im_rows = int(im_rows * 0.33)
+im_cols = int(im_cols * 0.33)
+
 
 # Split a multi-page image to multiple single-page images and save the converted images as BMP files.
 def open_multitiff(filename):
     img_pil = Image.open(filename)
-    photo_num =img_pil.n_frames
+    photo_num = img_pil.n_frames
     img = []
     count = 0
     while count < photo_num:
@@ -57,21 +59,24 @@ def open_multitiff(filename):
 
     return img
 
-def save_all_frames(video_path, dir_path, basename, ext='bmp'):
+
+def save_all_frames(video_path, dir_path, basename, ext="bmp"):
     cap = open_multitiff(video_path)
     os.makedirs(dir_path, exist_ok=True)
     base_path = os.path.join(dir_path, basename)
 
-    for i in range (0, len(cap)):
-      photo = cap[i]
-      photo = cv2.cvtColor(photo, cv2.COLOR_RGB2BGR)  
-      photo_resize = cv2.resize(photo, dsize=None, fx=0.33, fy=0.33)
-      num_004d = "{0:04d}".format(i)
-      print(num_004d)
-      img = dir_path + "/" + basename + "_" + str(num_004d) + ".bmp"
-      cv2.imwrite(img, photo_resize)
+    for i in range(0, len(cap)):
+        photo = cap[i]
+        photo = cv2.cvtColor(photo, cv2.COLOR_RGB2BGR)
+        photo_resize = cv2.resize(photo, dsize=None, fx=0.33, fy=0.33)
+        num_004d = "{0:04d}".format(i)
+        print(num_004d)
+        img = dir_path + "/" + basename + "_" + str(num_004d) + ".bmp"
+        cv2.imwrite(img, photo_resize)
 
-for vol in range(0,5):
-  NAME = BASENAME + str(vol)
-  save_all_frames(PATH + NAME + '.tif', '/content/drive/My Drive/data/' + NAME + "_new", NAME)
 
+for vol in range(0, 5):
+    NAME = BASENAME + str(vol)
+    save_all_frames(
+        PATH + NAME + ".tif", "/content/drive/My Drive/data/" + NAME + "_new", NAME
+    )
--- 02_mask.py
+++ 02_mask.py
@@ -1,91 +1,193 @@
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# Created on Tue Apr  5 19:21:38 2022
+
+# @author: tanakayudai
+
+# Generate a mask for a mouse.
+# """
+
+# # Import modules
+
+# # assert torch.__version__.startswith("1.8")
+# import json
+# import os
+# import random
+
+# # from google.colab.patches import cv2_imshow
+# # when running locally, use below instead
+# import cv2
+# import detectron2
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# #!pip install pyyaml==5.1
+# import torch
+# from detectron2.utils.logger import setup_logger
+# from PIL import Image
+
+# #!pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
+# # the code did not work for mac, trying with this instead pip install 'git+https://github.com/facebookresearch/detectron2.git'
+
+
+# setup_logger()
+# from detectron2 import model_zoo
+# from detectron2.config import get_cfg
+# from detectron2.data import DatasetCatalog, MetadataCatalog
+# from detectron2.engine import DefaultPredictor
+# from detectron2.utils.visualizer import Visualizer
+
+# # normal
+# PATH = "/Users/annastuckert/Downloads/Tanaka_et_al_2023/normal"
+# PATH_processed = "/Users/annastuckert/Downloads/Tanaka_et_al_2023/normal_mask"
+
+# NAME = "normal"
+
+# im_rows = int(600 * 0.33)
+# im_cols = int(700 * 0.33)
+
+
+# # These elements were just left as open a = (for some reason)
+# # pix =
+# # a =
+# # dif =
+# # p =
+
+# f = os.listdir(PATH + "/")
+# photo_num = len(f)
+
+
+# def rgb_to_gray(src):
+#     b, g, r = src[:, :, 0], src[:, :, 1], src[:, :, 2]
+
+#     return np.array(0.2989 * r + 0.5870 * g + 0.1140 * b, dtype="uint8")
+
+
+# im = cv2.imread(PATH + "/" + NAME + ".bmp")
+
+# # Recognize a mouse with a panoptic segmentation model
+# cfg = get_cfg()
+# cfg.merge_from_file(
+#     model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+# )
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+#     "COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"
+# )
+# predictor = DefaultPredictor(cfg)
+# panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+# v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+# out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+
+# TARGET = []
+# CLASSES = []
+# MASKS = []
+
+# panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+# v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+# v = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+# im_ps = v.get_image()[:, :, ::-1]
+# target = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes.index("cat")
+# TARGET.append(target)
+# outputs = predictor(im)
+# classes = np.asarray(outputs["instances"].to("cpu").pred_classes)
+# CLASSES.append(classes)
+# masks = np.asarray(outputs["instances"].to("cpu").pred_masks)[
+#     CLASSES[0] == TARGET[0]
+# ].astype("uint8")
+# MASKS.append(masks)
+# im_tra = MASKS[0].transpose(1, 2, 0)[:, :, 0]
+# im_tra_inv = -(im_tra - 1)
+# im_mask_inv = im_tra_inv.reshape(im_rows, im_cols, 1)
+# hige = im * im_mask_inv
+# im_mask = im_tra.reshape(im_rows, im_cols, 1)
+# im_masked = im * im_mask
+# back = np.zeros((im_rows, im_cols, 3))
+# back += [200, 167, 122][::-1]
+# im_invmask = im_mask * 255
+# im_invmask = cv2.bitwise_not(im_invmask)
+# im_invmask = im_invmask / 255
+# im_invmask = im_invmask.reshape(im_rows, im_cols, 1)
+# back_invmasked = back * im_invmask
+# mouse = im_masked + back_invmasked
+# cv2.imwrite("PATH", mouse)
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr  5 19:21:38 2022
 
-@author: tanakayudai
+Updated script to generate masks for mouse images.
+Handles sequential files and uses CPU for processing.
 
-Generate a mask for a mouse.
+@author: tanakayudai
 """
 
 # Import modules
-!pip install pyyaml==5.1
-import torch
-assert torch.__version__.startswith("1.8")
-!pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.8/index.html
+import os
+
+import cv2
 import numpy as np
-from PIL import Image
-import os, json, cv2, random
-import matplotlib.pyplot as plt
-from google.colab.patches import cv2_imshow
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
 from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.logger import setup_logger
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
 
-PATH = '*****'
-PATH_processed = '*****'
+# Initialize logger
+setup_logger()
 
-NAME = "*****"
+# Paths
+PATH = "/Users/annastuckert/Downloads/Tanaka_et_al_2023/normal"
+PATH_processed = "/Users/annastuckert/Downloads/Tanaka_et_al_2023/normal_mask"
 
-im_rows = int(600*0.33)
-im_cols = int(700*0.33)
+# Ensure output directory exists
+os.makedirs(PATH_processed, exist_ok=True)
 
-pix =
-a =
-dif =
-p =
+# Image dimensions (resize factors)
+im_rows = int(600 * 0.33)
+im_cols = int(700 * 0.33)
 
-f = os.listdir(PATH + "/")
-photo_num =  len(f)
-
-def rgb_to_gray(src):
-  b, g, r = src[:,:,0], src[:,:,1], src[:,:,2]
-
-  return np.array(0.2989 * r + 0.5870 * g + 0.1140 * b, dtype='uint8')
-
-im = cv2.imread(PATH + "/" + NAME+ ".bmp")
-
-# Recognize a mouse with a panoptic segmentation model
+# Configure Detectron2 to use CPU
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+cfg.merge_from_file(
+    model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+)
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"
+)
+cfg.MODEL.DEVICE = "cpu"  # Force CPU usage
+
+# Initialize predictor
 predictor = DefaultPredictor(cfg)
-panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
-v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
 
-TARGET = []
-CLASSES = []
-MASKS = []
+# Process all files in the directory
+for file_name in sorted(os.listdir(PATH)):
+    # Check if the file matches the expected pattern and is a .bmp file
+    if file_name.startswith("normal_") and file_name.endswith(".bmp"):
+        # Full path to the input file
+        input_path = os.path.join(PATH, file_name)
+        output_path = os.path.join(PATH_processed, file_name)
 
-panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
-v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-v = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
-im_ps = v.get_image()[:, :, ::-1]
-target = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes.index("cat")
-TARGET.append(target)
-outputs = predictor(im)
-classes = np.asarray(outputs["instances"].to("cpu").pred_classes)
-CLASSES.append(classes)
-masks = np.asarray(outputs["instances"].to("cpu").pred_masks)[CLASSES[0]==TARGET[0]].astype("uint8")
-MASKS.append(masks)
-im_tra = MASKS[0].transpose(1,2,0)[:,:,0]
-im_tra_inv = -(im_tra -1)
-im_mask_inv = im_tra_inv.reshape(im_rows, im_cols, 1)
-hige = im * im_mask_inv
-im_mask = im_tra.reshape(im_rows, im_cols, 1)
-im_masked = im * im_mask
-back = np.zeros((im_rows, im_cols, 3))
-back += [200,167,122][::-1]
-im_invmask = im_mask *255
-im_invmask = cv2.bitwise_not(im_invmask)
-im_invmask = im_invmask/255
-im_invmask = im_invmask.reshape(im_rows, im_cols,1)
-back_invmasked = back * im_invmask
-mouse = im_masked + back_invmasked
-cv2.imwrite("PATH", mouse)
+        # Read the image
+        im = cv2.imread(input_path)
+        if im is None:
+            print(f"Warning: Could not read image: {input_path}")
+            continue
+
+        # Perform panoptic segmentation
+        panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+
+        # Visualize the segmentation
+        visualizer = Visualizer(
+            im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2
+        )
+        visualized_output = visualizer.draw_panoptic_seg_predictions(
+            panoptic_seg.to("cpu"), segments_info
+        )
+        im_ps = visualized_output.get_image()[:, :, ::-1]
+
+        # Save the processed image
+        cv2.imwrite(output_path, im_ps)
+        print(f"Processed and saved: {output_path}")
